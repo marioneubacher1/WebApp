4dbe33ac2873e552de2cc46f340d4c3f0df9d1b8
marioneubacher1 <144902165+marioneubacher1@users.noreply.github.com>
2024-01-23 07:26:31 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,20 @@
+function submitForm() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Benutzerdaten aus dem LocalStorage abrufen
+    const storedUserData = JSON.parse(localStorage.getItem('userData'));
+
+    if (storedUserData && storedUserData.username === username) {
+        // Überprüfen, ob das eingegebene Passwort mit dem gespeicherten Passwort übereinstimmt (in der Praxis sollte das Passwort sicher gehasht sein)
+        if (storedUserData.password === password) {
+            // Hier könnten Sie die Anmeldung erfolgreich gestalten, z.B. eine Weiterleitung oder Anzeige eines Erfolgs-Icons
+            alert('Anmeldung erfolgreich!');
+            window.location.href = 'illustration.html';
+        } else {
+            alert('Falsches Passwort. Bitte erneut versuchen.');
+        }
+    } else {
+        alert('Benutzer nicht gefunden. Bitte registrieren Sie sich zuerst.');
+    }
+}
